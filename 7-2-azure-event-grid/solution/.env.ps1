360bcfe5e521c96030122735a02de8858ed52e58
Douglas Crawford <doug.a.crawford@gmail.com>
2026-08-24 15:59:00 -0500

    Complete 7-2 Azure Event Grid

---
+++ 7-2-azure-event-grid/solution/.env.ps1
@@ -0,0 +1,4 @@
+$env:RESOURCE_GROUP = "rg-azure-event-grid"
+$env:NAMESPACE_NAME = "egns-exercise-d37563cb"
+$env:EVENTGRID_TOPIC_NAME = "moderation-events"
+$env:EVENTGRID_ENDPOINT = "https://egns-exercise-d37563cb.centralus-1.eventgrid.azure.net"
