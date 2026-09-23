6b4a954220698e6b710df74bbf2b14a64ee4b2a5
Douglas Crawford <doug.a.crawford@gmail.com>
2026-09-22 21:57:38 -0500

    quick code service bus playground (caochang)

---
+++ playground/service-bus/app.py
@@ -0,0 +1,27 @@
+from azure.identity import DefaultAzureCredential
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+import os
+
+credential = DefaultAzureCredential()
+
+with ServiceBusClient(
+    fully_qualified_namespace=os.getenv("SERVICE_BUS_NAMESPACE"),
+    credential=credential
+) as client:
+    with client.get_queue_sender("inference-requests") as queue_sender:
+        queue_sender.send_messages(
+            ServiceBusMessage("queue message body")
+        )
+
+    with client.get_topic_sender("inference-results") as topic_sender:
+        topic_sender.send_messages(
+            ServiceBusMessage("topic message body")
+        )
+
+with client.get_subscription_receiver(
+    topic_name="inference-results",
+    subscription_name="notifications"
+) as receiver:
+    for msg in receiver:
+        print(str(msg))
+        receiver.complete_message(msg)
