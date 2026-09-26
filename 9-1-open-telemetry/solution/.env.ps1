287723697401695fdf29dd0742f4e1edf5e328c4
Douglas Crawford <doug.a.crawford@gmail.com>
2026-09-26 09:37:56 -0500

    7-3 solution but failed

--- 9-1-open-telemetry/solution/.env.ps1
+++ 9-1-open-telemetry/solution/.env.ps1
@@ -1,2 +1,2 @@
-$env:APPLICATIONINSIGHTS_CONNECTION_STRING = "InstrumentationKey=87c5a008-d98d-4c20-a8f2-48ce965b7faf;IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/;ApplicationId=57bafefe-6232-4a7c-8e3b-78ae70dd0d8b"
+$env:APPLICATIONINSIGHTS_CONNECTION_STRING = "InstrumentationKey=c472139a-589d-4649-962d-456419840308;IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/;ApplicationId=d1d55bc0-76d5-4490-877d-eaa8150cfc8d"
 $env:OTEL_SERVICE_NAME = "document-pipeline-app"
